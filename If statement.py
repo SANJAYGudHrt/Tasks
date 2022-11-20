8585d362237d48485eecb78d8362d169fd100fcb
Sanjay <sanjaymadasamy3@gmail.com>
2022-11-20 13:03:24 +0530

    Create If statement.py

---
+++ If statement.py
@@ -0,0 +1,9 @@
+A=int(input("Enter English number"))
+B=int(input("Enter Tamil number"))
+C=int(input("Enter Bio-zology number"))
+D=int(input("Enter Social number"))
+E=int(input("Enter Bio-botony number"))
+if (A>=50)and(B>=50)and(C>=50)and(D>=50)and(E>=50):
+    print("Pass")
+
+
